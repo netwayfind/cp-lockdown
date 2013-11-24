5e81c53cf5c82971cff98e6de49090f28ec75c85
Mark <sumwonyuno@gmail.com>
2013-11-23 19:29:07 -1000

    programmatic way to create guide
    a table of contents list contains all of the sections for a particular OS. the
    create script will search the /res directory for <section>-<label>.html files.
    if a section is not yet available, a TODO is added. this should make it easier
    to manage sections and their contents among OS versions.

--- create.py
+++ create.py
@@ -1,21 +1,32 @@
 import os
 
+windows_toc = [
+    'Account Lockout Policy',
+    'Administrative Tools'
+    ]
+
 def get_contents(file_path):
     contents = ""
-    with open(file_path, "r") as file:
-        for line in file.readlines():
-            contents += line
+    try:
+        with open(file_path, "r") as file:
+            for line in file.readlines():
+                contents += line
+    except IOError:
+        # if error, assume that file doesn't exist
+        # when expected file is created, its contents will be used
+        # until then, put TODO placeholder
+        contents = "TODO"
     return contents
 
 def res_path(file_prefix, version):
     return "res" + os.sep + file_prefix + "-" + version + ".html"
 
-def section(name, win_ver, output_file):
+def section(name, version, output_file):
     id = name.lower().replace(" ", "-")
     output_file.write("<hr>\n")
     output_file.write("<div id=\"" + id + "\">\n")
     output_file.write("<h2>" + name + "</h2>\n")
-    output_file.write(get_contents(res_path(id, win_ver)))
+    output_file.write(get_contents(res_path(id, version)))
     output_file.write("</div>\n")
 
 def for_windows_version(full_name, short_name):
@@ -27,8 +38,8 @@ def for_windows_version(full_name, short_name):
         file.write("</head>\n\n")
         file.write("<body>\n")
         file.write("<h1>" + full_name + "</h1>\n")
-        section("Account Lockout Policy", short_name, file)
-        section("Administrative Tools", short_name, file)
+        for topic in windows_toc:
+            section(topic, short_name, file)
         file.write("</body>\n")
         file.write("</html>")
 
