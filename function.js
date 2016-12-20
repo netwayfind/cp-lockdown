72e23580447bd6910a44057917c0f8d7900a2bd8
Mark <mmmunar@hawaii.edu>
2016-12-19 19:05:21 -1000

    convert win7 to new way
    split up sections into separate files, then use javascript to load files.
    JSON config to set up order. this should make it easier to update entries.

---
+++ function.js
@@ -0,0 +1,53 @@
+function getJSON(path) {
+  console.log(path);
+  var req = new XMLHttpRequest();
+  req.overrideMimeType("application/json");
+  req.open("GET", path, false);
+  req.send(null);
+  return JSON.parse(req.responseText);
+}
+
+function getSection(path) {
+  console.log("  " + path);
+  var req = new XMLHttpRequest();
+  req.overrideMimeType("text/html");
+  req.open("GET", path, false);
+  req.send(null);
+  return req.responseText;
+}
+
+function load(path) {
+  var config = getJSON(path);
+  var toc = "";
+  var content = "";
+  for (i = 0; i < config.length; i++) {
+    var group = config[i];
+    var groupName = group.group;
+    console.log(groupName);
+    toc += groupName;
+    toc += "<ul>\n";
+    content += "<h1>";
+    content += groupName;
+    content += "</h1>\n<hr>\n";
+    var sections = group.sections;
+    for (j = 0; j < sections.length; j++) {
+      var section = sections[j];
+      var sectionName = section.section;
+      console.log(" " + sectionName);
+      toc += "<li><a href=\"#";
+      toc += sectionName;
+      toc += "\">";
+      toc += sectionName;
+      toc += "</a></li>\n";
+      content += "<div id=\"";
+      content += sectionName;
+      content += "\">\n"
+      content += getSection(section.path);
+      content += "\n<a href=\"#toc\">Table of Contents</a>\n";
+      content += "</div>\n<hr>\n";
+    }
+    toc += "</ul>\n";
+  }
+  document.getElementById("toc").innerHTML = toc;
+  document.getElementById("content").innerHTML = content;
+}
